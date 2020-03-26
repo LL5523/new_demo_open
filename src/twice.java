95cd7253a9f69b02bcf744b3238b5653a0a3984b
ll5523 <ll5523@email.com>
2020-03-26 16:54:48 +0800

    pull hot

--- src/twice.java
+++ src/twice.java
@@ -2,6 +2,6 @@
 public class twice {
 	public static void main(String[] args) {
 		System.out.println("第二次好坏操作");
->>>>>>> branch 'master' of https://github.com/LL5523/new_demo_open.git
+		System.out.println("hot fix");
 	}
 }
