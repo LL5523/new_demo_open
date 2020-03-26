1f2454c325042845772841e44a6bb7126ad5b1ba
ll5523 <ll5523@email.com>
2020-03-26 16:28:14 +0800

    goodbad

--- src/twice.java
+++ src/twice.java
@@ -1,10 +1,7 @@
 
 public class twice {
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("第二次坏操作");
-=======
-		System.out.println("第二次好操作");
+		System.out.println("第二次好坏操作");
 >>>>>>> branch 'master' of https://github.com/LL5523/new_demo_open.git
 	}
 }
